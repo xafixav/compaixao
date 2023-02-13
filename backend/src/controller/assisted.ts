59680387d8fd77dd3de9ed5f9446a6f02ab6cf3e
xafixav <xafiworld@gmail.com>
2023-02-13 16:54:54 -0300

    :white_check_mark: assisted controller

---
+++ backend/src/controller/assisted.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { AssistedService } from '../service/assisted/index';
+
+export default class AssistedController {
+  private assistedService: typeof AssistedService;
+
+  constructor() {
+    this.assistedService = AssistedService;
+  }
+
+  public register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { assistedId, cpf, description, livingState, name, rg } = req.body;
+
+      const assistedResponse = await this.assistedService.register({ assistedId, cpf, description, livingState, name, rg });
+
+      if (assistedResponse) {
+        return res.status(StatusCodes.OK).json(assistedResponse);
+      }
+
+    } catch (e) {
+      next(e);
+    }
+  };
+}
