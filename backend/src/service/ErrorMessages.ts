f2e5521a8eaf2ee84e9a6cdaafb7218fe3057893
xafixav <xafiworld@gmail.com>
2023-02-13 16:54:38 -0300

    :white_check_mark: error manager/messages

---
+++ backend/src/service/ErrorMessages.ts
@@ -0,0 +1,9 @@
+const ERROR_MESSAGES = {
+    CPF_AND_RG_MISSING: 'CPF e RG nao informados',
+    ASSISTED_ID_MISSING: 'Numero de pulseira não informada',
+    ASSISTED_NAME_MISSING: 'Nome do assistenciado não informada',
+    ASSISTED_LIVING_STATE_MISSING: 'Moradia do assistenciado não informado',
+    ASSISTED_REGISTER_FAILURE: 'Falha ao registrar o assistenciado',
+}
+
+export default ERROR_MESSAGES;
