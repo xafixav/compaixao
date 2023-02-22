97e7623ad2f0e806a6630fb5c94bfb933d765f19
Xafixav <xafiworld@gmail.com>
2023-02-22 19:33:24 -0300

    :white_check_mark: add new error messages

--- backend/src/service/ErrorMessages.ts
+++ backend/src/service/ErrorMessages.ts
@@ -1,9 +1,17 @@
 const ERROR_MESSAGES = {
-    CPF_AND_RG_MISSING: 'CPF e RG nao informados',
-    ASSISTED_ID_MISSING: 'Numero de pulseira não informada',
-    ASSISTED_NAME_MISSING: 'Nome do assistenciado não informada',
-    ASSISTED_LIVING_STATE_MISSING: 'Moradia do assistenciado não informado',
-    ASSISTED_REGISTER_FAILURE: 'Falha ao registrar o assistenciado',
-}
+	CPF_AND_RG_MISSING: 'CPF e RG nao informados',
+	CPF_MUST_BE_11_DIGITS: 'CPF deve ter 11 digitos',
+	ASSISTED_ID_MISSING: 'Numero de pulseira não informada',
+	ASSISTED_NAME_MISSING: 'Nome do assistenciado não informada',
+	ASSISTED_LIVING_STATE_MISSING: 'Moradia do assistenciado não informado',
+	ASSISTED_REGISTER_FAILURE: 'Falha ao registrar o assistenciado',
+	ASSISTED_BORN_CITY_MISSING: 'Cidade de nascimento do assistenciado não informada',
+	ASSISTED_BORN_STATE_MISSING: 'Cidade de nascimento do assistenciado não informada',
+	ASSISTED_BORN_AGE_MISSING: 'Data de nascimento do assistenciado não informado',
+	PRODUCT_SIZE_NOT_INFORMED: 'Tamanho do produto não informado.',
+	PRODUCT_QUANTITY_NOT_INFORMED: 'Quantidade do produto não informado.',
+	PRODUCT_TYPE_NOT_INFORMED: 'Tipo do produto não informado.',
+	PRODUCT_GENDER_NOT_INFORMED: 'Genero do produto não informado.',
+};
 
 export default ERROR_MESSAGES;
