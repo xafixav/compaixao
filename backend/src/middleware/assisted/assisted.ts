c36057adfeb5c6923b4847e17db3c3e30b2100fd
Xafixav <xafiworld@gmail.com>
2023-02-22 19:32:53 -0300

    :white_check_mark: new middlewares

---
+++ backend/src/middleware/assisted/assisted.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import NewAssistedSchema from '../../service/schema/newAssistedSchema';
+
+export default class AssistedMiddleware {
+	private started: boolean;
+
+	constructor() {
+		this.started = false;
+	}
+
+	private start = () => {
+		if (!this.started) {
+			this.started = true;
+		}
+	};
+
+	public newAssistedIsValid = (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { assistedId, cpf, livingState, name, bornAge, bornCity, bornState, jobProfession } = req.body;
+  
+			const { error } = NewAssistedSchema.defaultSchema.validate({
+				assistedId, cpf, livingState, name, bornAge, bornCity, bornState, jobProfession
+			}, { convert: false });
+  
+			if (error) {
+				const [StatusCode, ErrorMessage] = error.details[0].message.split('|');
+				return res.status(Number(StatusCode)).json({ message: ErrorMessage });
+			}
+  
+			next();
+
+		} catch(e: any) {
+			return res.status(StatusCodes.BAD_REQUEST).json({ message: e.message });
+		}
+	};
+}
