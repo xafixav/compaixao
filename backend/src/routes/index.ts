8dd99fe368b0f3d5c6c31e0c2c45664e77818167
Xafixav <xafiworld@gmail.com>
2023-03-04 04:53:10 -0300

    :white_check_mark: inv and rel inv routers

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,6 @@
 import assistedRouter from './assisted';
 import comentaryRouter from './comentary';
+import inventoryRouter from './inventory';
+import relationalInventoryRouter from './relationalInventory';
 
-export default [assistedRouter, comentaryRouter];
+export default [assistedRouter, comentaryRouter, inventoryRouter, relationalInventoryRouter];
