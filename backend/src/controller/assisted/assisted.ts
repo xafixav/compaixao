a121b8773d30f56325ea136a01241c29d970ae1f
Xafixav <xafiworld@gmail.com>
2023-03-03 23:51:06 -0300

    :white_check_mark: update assisted and create comentary

---
+++ backend/src/controller/assisted/assisted.ts
@@ -0,0 +1,109 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { AssistedService } from '../../service/assisted/index';
+
+export default class AssistedController {
+	private assistedService: typeof AssistedService;
+
+	constructor() {
+		this.assistedService = AssistedService;
+	}
+
+	public register = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { 
+				assistedNumber,
+				cpf,
+				livingState,
+				name,
+				bornAge,
+				bornCity,
+				bornState,
+				jobProfession,
+				gender,
+				legsNumber,
+				shirtNumber,
+				shoesNumber
+			} = req.body;
+
+			const assistedResponse = await this.assistedService.register({ 
+				assistedNumber,
+				cpf,
+				livingState,
+				name,
+				bornAge,
+				bornCity,
+				bornState,
+				jobProfession,
+				gender,
+				legsNumber,
+				shirtNumber,
+				shoesNumber
+			});
+
+			if (assistedResponse) {
+				return res.status(StatusCodes.OK).json(assistedResponse);
+			}
+
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	public getAll = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+
+			const assistedResponse = await this.assistedService.getAll();
+
+			if (assistedResponse) {
+				return res.status(StatusCodes.OK).json(assistedResponse);
+			}
+
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	public update = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const {
+				id,
+				assistedNumber,
+				cpf,
+				livingState,
+				name,
+				bornAge,
+				bornCity,
+				bornState,
+				jobProfession,
+				gender,
+				legsNumber,
+				shirtNumber,
+				shoesNumber
+			} = req.body;
+
+			const assistedResponse = await this.assistedService.update({
+				id,
+				assistedNumber,
+				cpf,
+				livingState,
+				name,
+				bornAge,
+				bornCity,
+				bornState,
+				jobProfession,
+				gender,
+				legsNumber,
+				shirtNumber,
+				shoesNumber
+			});
+
+			if (assistedResponse) {
+				return res.status(StatusCodes.OK).json(assistedResponse);
+			}
+
+		} catch (e) {
+			next(e);
+		}
+	};
+}
