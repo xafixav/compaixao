d69e34663f03a0323b03552718e38facf645b174
Xafixav <xafiworld@gmail.com>
2023-03-12 23:38:04 -0300

    :white_check_mark: add API Context

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -7,19 +7,29 @@ const api = axios.create({
 });
 
 const getAllAssisted = async () => {
-    // const fetchRequest = await fetch(`${address}/assisted/getall`);
-    const { data } = await api.get('/assisted/getall');
-    return data;
-}
+  // const fetchRequest = await fetch(`${address}/assisted/getall`);
+  const { data } = await api.get('/assisted/getall');
+  return data;
+};
 
 const registerAssisted = async (object) => {
-    const { data } = await api.post('/assisted/register', object);
-    return data;
-}
+  const { data } = await api.post('/assisted/register', object);
+  return data;
+};
+
+const registerAssistedComentary = async (object) => {
+  const { data } = await api.post('/assisted/comentary/register', object);
+  return data;
+};
 
 const registerProduct = async (object) => {
   const { data } = await api.post('/assisted/register', object);
   return data;
-}
+};
 
-export { getAllAssisted, registerAssisted, registerProduct }
+export { 
+  getAllAssisted, 
+  registerAssisted, 
+  registerProduct, 
+  registerAssistedComentary 
+};
