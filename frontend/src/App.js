eb2dc16a1399e59d7116716bee5e8cc63bc100a4
Xafixav <xafiworld@gmail.com>
2023-03-12 23:35:46 -0300

    :white_check_mark: add API Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
-import {   BrowserRouter as Router,
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
   Routes,
   Route,
-  Link } from 'react-router-dom'
+} from 'react-router-dom';
 import './App.css';
-import Forms from './components/Forms';
-import AssistedInfo from './components/AssistedInfo'
-import Formulary from './components/Formulary';
+// import Forms from './components/Forms';
+import AssistedInfo from './components/AssistedInfo';
+// import Formulary from './components/Formulary';
+import AssistedForms from './components/AssistedForms';
 import Header from './components/Header';
+import { AssistedContext } from './services/AssistedContext';
 
 function App() {
+
   return (
+    <AssistedContext.Provider value={{}}>
     <Router>
       {Header()}
-        <Routes>
-          <Route exact path='/' element={<Formulary/>}/>
-          <Route exact path='/assisted' element={<AssistedInfo/>}/>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<AssistedForms />} />
+        <Route exact path="/assisted" element={<AssistedInfo />} />
+      </Routes>
     </Router>
+    </AssistedContext.Provider>
   );
 }
 
