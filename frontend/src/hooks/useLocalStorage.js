f201274a55d90fc6666a29127c781917aca6dab9
Xafixav <xafiworld@gmail.com>
2023-05-20 17:23:35 -0300

    :construction: updating project to localStorage + context methods

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,20 +1,34 @@
 import {useState, useEffect} from 'react';
 
 function useLocalStorage() {
-  const [assisted, setAssisted] = useState([]);
-  const storageAssisted = localStorage.getItem('assistedInfo');
-  const initialValue = [];
-  const allAssisteds = storageAssisted !== null ? JSON.parse(storageAssisted) : undefined;
-  if (typeof allAssisteds === 'undefined') {
-    localStorage.setItem('assistedInfo', JSON.stringify(initialValue));
+  const [assistedsArr, setAssisteds] = useState([]);
+
+  const saveAssisteds = () => {
+    localStorage.setItem('assistedInfo', JSON.stringify(assistedsArr));
   }
-  const saveAssisteds = (assisted) => {
-    setAssisted(assisted);
-    localStorage.setItem('assistedInfo', JSON.stringify(assisted));
+
+  const getAssisteds = () => {
+    const storageAssisted = localStorage.getItem('assistedInfo');
+    if (Array.isArray(storageAssisted)) {
+      setAssisteds(JSON.parse(storageAssisted));
+    }
   }
+
+  useEffect(() => {
+    getAssisteds();
+  }
+  , []);
+
+  useEffect(() => {
+    saveAssisteds();
+  }, [assistedsArr]);
+
+  return [assistedsArr, setAssisteds];
 
   // TODO -> Atualizar o site para não fazer uso do backend, as informações serão salvas no localstorage e pastebin usando a API.
   // Terminar -> Terminar o sistema de salvar as informações no localStorage. É necessario antes de tudo entender a nova estrutura de dados dos assistenciados.
   // A parte de pedidos de oração e comentarios deve estar dentro do objeto do assistenciado, em uma chave nova da qual vai ser um array de objetos? A decidir.
   
 }
+
+export default useLocalStorage;
